fd7068c9e0c0e7632f7c109ea44c31b5931fc59b
Michael Woodruff <michael@ikky.com>
2016-04-30 12:32:49 +0800

    add Elo variable and documentation

--- src/main/java/com/riotgames/interview/hongkong/matchmaking/Player.java
+++ src/main/java/com/riotgames/interview/hongkong/matchmaking/Player.java
@@ -20,6 +20,32 @@ public class Player {
     private final long losses;
     private final float winRatio;
 
+    /**
+     * <p> Elo rating of the player </p>
+     * Ref: https://en.wikipedia.org/wiki/Elo_rating_system
+     *
+     * Note: In this assignment, the players have already played (wins + losses) amount of games.
+     * Elo ratings are generally generated and incremented / decremented from the the player's first game based on who they playe.
+     *
+     * If the player faces a difficult opponent / team and they win they are rewarded more points. Conversely, if the player faces an opponent / team
+     * where they are expected to lose, they will lose more points.
+     * 
+     *
+     * However, this assignment does not have any rating data or 'Match History' data. Therefore it's impossible to calculate the Elo rating for this player
+     * based on just the wins and losses (who knows if the player won many easy games or many hard games? The rating would be different in each case).
+     *
+     * Therefore, the initial Elo rating will be quite 'dumb' and be based on the following calculation:
+     *
+     * PlayerInitialElo = 1000 * WinRatio
+     *
+     * Where 1000 represents a base Rating (for example a player with 0 games, or 100 wins and 100 losses, should have a rating of 1000
+     *
+     * Afterwards, the Elo Rating will be updated in the proper way whereby more points are awarded / deducted depending on how 'surprising'
+     * the result is
+     *
+     */
+    private int eloRating;
+
     public Player(String name, long wins, long losses) {
         this.name = name;
         this.wins = wins;
